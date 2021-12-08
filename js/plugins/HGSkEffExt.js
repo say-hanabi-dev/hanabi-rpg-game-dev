f6edf919cf38319c47a950454d43bc056673dbf9
cch6o <xiaomingxmxm20@gmail.com>
2021-12-07 20:20:48 -0500

    1207 lars 群更新 merge1 + minor fix

--- js/plugins/HGSkEffExt.js
+++ js/plugins/HGSkEffExt.js
@@ -112,27 +112,27 @@ Game_Battler.prototype.removeStatesAuto = function(timing){
     HGSkEffExt._GameBattler_removeStatesAuto.call(this, timing);
 };
 
-HGSkEffExt.condDfAbstId = [//state dependent defense absence
-    {skId: 56, stId: 23, addFormula: "+ b.mdf * 2"}
-];
-HGSkEffExt._GameAction_evalDamageFormula = Game_Action.prototype.evalDamageFormula;
-Game_Action.prototype.evalDamageFormula = function(target){
-    try{
-        let res =  HGSkEffExt._GameAction_evalDamageFormula.call(this, target);
-        for(let i = 0; i<HGSkEffExt.condDfAbstId.length; i++){
-            if((DataManager.isSkill(this.item())) && (this.item().id == HGSkEffExt.condDfAbstId[i].skId) 
-            && (target.isStateAffected(HGSkEffExt.condDfAbstId[i].stId))){
-                var item = this.item();
-                var a = this.subject();
-                var b = target;
-                var v = $gameVariables._data;
-                var sign = ([3, 4].contains(item.damage.type) ? -1 : 1);
-                return Math.max(eval(item.damage.formula+(HGSkEffExt.condDfAbstId[i].addFormula)), 0) * sign;
-            }
-        }
-    }catch(e){
-        return 0;
-    }
-};
+// HGSkEffExt.condDfAbstId = [//state dependent defense absence
+//     {skId: 56, stId: 23, addFormula: "+ b.mdf * 2"}
+// ];
+// HGSkEffExt._GameAction_evalDamageFormula = Game_Action.prototype.evalDamageFormula;
+// Game_Action.prototype.evalDamageFormula = function(target){
+//     try{
+//         let res =  HGSkEffExt._GameAction_evalDamageFormula.call(this, target);
+//         for(let i = 0; i<HGSkEffExt.condDfAbstId.length; i++){
+//             if((DataManager.isSkill(this.item())) && (this.item().id == HGSkEffExt.condDfAbstId[i].skId) 
+//             && (target.isStateAffected(HGSkEffExt.condDfAbstId[i].stId))){
+//                 var item = this.item();
+//                 var a = this.subject();
+//                 var b = target;
+//                 var v = $gameVariables._data;
+//                 var sign = ([3, 4].contains(item.damage.type) ? -1 : 1);
+//                 return Math.max(eval(item.damage.formula+(HGSkEffExt.condDfAbstId[i].addFormula)), 0) * sign;
+//             }
+//         }
+//     }catch(e){
+//         return 0;
+//     }
+// };
 
 HGSkEffExt.poiStId = 44;
