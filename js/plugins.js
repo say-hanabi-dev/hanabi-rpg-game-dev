27bcd39541dad8b0226807a3df93d6ce1ec78455
cch6o <xiaomingxmxm20@gmail.com>
2022-01-06 21:32:55 -0500

    自动存档间隔改为一分钟

--- js/plugins.js
+++ js/plugins.js
@@ -6,7 +6,7 @@ var $plugins =
 {"name":"HGDlyMxPp","status":true,"description":"Daily maximum prop limit","parameters":{}},
 {"name":"HGSpItm","status":true,"description":"Equipment Rarity Enhancement Card by c2h6o for Hanabi Gakuen","parameters":{}},
 {"name":"YEP_MessageCore","status":true,"description":"v1.02 Adds more features to the Message Window to customized\nthe way your messages appear and functions.","parameters":{"---General---":"","Default Rows":"4","Default Width":"Graphics.boxWidth","Face Indent":"Window_Base._faceWidth + 24","Fast Forward":"Input.isPressed('pagedown')","Word Wrapping":"false","Description Wrap":"false","---Font---":"","Font Name":"GameFont","Font Size":"28","Font Size Change":"12","Font Changed Max":"96","Font Changed Min":"12","---Name Box---":"","Name Box Buffer X":"-28","Name Box Buffer Y":"0","Name Box Padding":"this.standardPadding() * 4","Name Box Color":"0","Name Box Clear":"false","Name Box Added Text":"\\c[6]"}},
-{"name":"HGPlgCore","status":true,"description":"Core of plugins by c2h6o for Hanabi Gakuen","parameters":{"localTesting":"1"}},
+{"name":"HGPlgCore","status":true,"description":"Core of plugins by c2h6o for Hanabi Gakuen","parameters":{"localTesting":"0"}},
 {"name":"HGSkEffExt","status":true,"description":"Skill Effect Extension by c2h6o for Hanabi Gakuen","parameters":{}},
 {"name":"HGSkEqpRel","status":true,"description":"Relationships of Skills & Equipments by c2h6o for Hanabi Gakuen","parameters":{}},
 {"name":"HGSkLn","status":true,"description":"Skill learning by c2h6o for Hanabi Gakuen","parameters":{}},
