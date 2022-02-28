c4decad2494a937e8c699e1dd489d6e6aa30773b
lars <95558542+akuel@users.noreply.github.com>
2022-02-28 16:48:50 +0800

    2.28测试

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -95,7 +95,8 @@ DataManager.loadDataFile = function(name, src) {
             if (xhr.status < 400) {
                 if(name ==='$dataActors'){
                     var actors = [];
-                    json ={"id":dataActors.data.uid,
+                    json ={[
+                           {"id":dataActors.data.uid,
                            "battlerName":"Actor1_1",
                            "characterIndex":0,
                            "characterName":"Actor1",
@@ -107,10 +108,12 @@ DataManager.loadDataFile = function(name, src) {
                            "initialLevel":1,
                            "maxLevel":9999,
                            "name":dataActors.data.username,
-                           "nickname":"",
+                           "nickname":"测试2.28",
                            "note":"",
                            "profile":"",
-                           "gold":dataActors.data.coin}; 
+                           "gold":dataActors.data.coin},
+                           {"id":2,"battlerName":"Actor1_2","characterIndex":1,"characterName":"Actor1","classId":1,"equips":[0,0,0,0,0],"faceIndex":1,"faceName":"Actor1","traits":[],"initialLevel":1,"maxLevel":80,"name":"伙伴1","nickname":"","note":"","profile":""}
+                           ]}; 
                     actors.push(null);
                     actors.push(json);
                     console.log(actors);                    
