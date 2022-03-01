d8ee6f0dbb7befbc2300cda0313c8e9c007af531
lars <95558542+akuel@users.noreply.github.com>
2022-03-01 12:19:35 +0800

    3.1

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -85,7 +85,7 @@ DataManager.loadDataFile = function(name, src) {
         xhr.open('GET', url);
         xhr.onload = function() {
             console.log(xhr.responseText);
-            var json = {};
+            //var json = {};
             var dataActors = {};
             try{
                 dataActors = JSON.parse(xhr.responseText)
@@ -95,8 +95,7 @@ DataManager.loadDataFile = function(name, src) {
             if (xhr.status < 400) {
                 if(name ==='$dataActors'){
                     var actors = [];
-                    json ={[
-                           {"id":dataActors.data.uid,
+                    /*json ={"id":dataActors.data.uid,
                            "battlerName":"Actor1_1",
                            "characterIndex":0,
                            "characterName":"Actor1",
@@ -111,22 +110,7 @@ DataManager.loadDataFile = function(name, src) {
                            "nickname":"测试1",
                            "note":"",
                            "profile":"",
-                           "gold":dataActors.data.coin},
-                           {"id":dataActors.data.uid+1,
-                           "battlerName":"Actor1_1",
-                           "characterIndex":0,
-                           "characterName":"Actor1",
-                           "classId":1,
-                           "equips":[1,1,2,3,0],
-                           "faceIndex":0,
-                           "faceName":"A",
-                           "traits":[],
-                           "initialLevel":1,
-                           "maxLevel":9999,
-                           "name":dataActors.data.username,
-                           "nickname":"测试2",
-                           "note":"",
-                           "profile":""}]}; 
+                           "gold":dataActors.data.coin}; */
                     actors.push(null);
                     actors.push(json);
                     console.log(actors);                    
