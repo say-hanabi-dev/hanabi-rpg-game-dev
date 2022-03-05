469294dc2e7da12dba56a1d947f2255893151525
lars <95558542+akuel@users.noreply.github.com>
2022-03-05 20:30:02 +0800

    loading测试

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -1610,7 +1610,7 @@ SceneManager.initGraphics = function() {
     Graphics.initialize(this._screenWidth, this._screenHeight, type);
     Graphics.boxWidth = this._boxWidth;
     Graphics.boxHeight = this._boxHeight;
-    Graphics.setLoadingImage('img/system/Loading.png');
+    Graphics.setLoadingImage('img/system/Loading.gif');
     if (Utils.isOptionValid('showfps')) {
         Graphics.showFps();
     }
