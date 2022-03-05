794d13d85a1e250f347c66ec4ab753ee6755b248
lars <95558542+akuel@users.noreply.github.com>
2022-03-05 20:45:27 +0800

    还原

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -1610,7 +1610,7 @@ SceneManager.initGraphics = function() {
     Graphics.initialize(this._screenWidth, this._screenHeight, type);
     Graphics.boxWidth = this._boxWidth;
     Graphics.boxHeight = this._boxHeight;
-    Graphics.setLoadingImage('img/system/Loading.gif');
+    Graphics.setLoadingImage('img/system/Loading.png');
     if (Utils.isOptionValid('showfps')) {
         Graphics.showFps();
     }
